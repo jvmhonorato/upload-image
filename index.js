5046166d4f6b905967c142e3e950280707e3385f
Victor Honorato <101150943+vituhonorato@users.noreply.github.com>
2022-09-26 19:37:14 -0300

    upload image

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const multer = require('multer')
+
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/')
+    },
+    filename: (req, file, cb) => {
+       cb(null, Date.now()+'-'+file.originalname)
+    }
+})
+const upload = multer({storage})
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res)=> {
+    res.render('home')
+})
+
+//generate a image on upload folder
+app.post('/', upload.single('img'), (req, res)=> {
+    console.log(req.body, req.file)
+    res.send('ok')
+})
+
+
+app.listen(3000, ()=> {
+    console.log('Server runnig')
+})
